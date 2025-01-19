7415a3846e310dcc5f0183c8699b838a44cf2ea3
Durgesh-Gupta <durgesh10gupta@gmail.com>
2025-01-19 19:27:38 +0530

    feat:Added Favicon and Updated sequence of sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { motion, useScroll } from 'framer-motion';
-import Navbar from './components/Navbar/Navbar';
-import Banner from './components/Banner';
-import About from './components/About/About';
-import Skills from './components/Skills/Skills';
-import Timeline from './components/Timeline/Timeline';
-import Projects from './components/Projects/Projects';
-import Connect from './components/Connect/Connect';
-import { useScrollSound } from './hooks/useScrollSound';
+import React from "react";
+import { motion, useScroll } from "framer-motion";
+import Navbar from "./components/Navbar/Navbar";
+import Banner from "./components/Banner";
+import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
+import Timeline from "./components/Timeline/Timeline";
+import Projects from "./components/Projects/Projects";
+import Connect from "./components/Connect/Connect";
+import { useScrollSound } from "./hooks/useScrollSound";
 
 function App() {
   const { scrollYProgress } = useScroll();
@@ -19,13 +19,12 @@ function App() {
         className="fixed top-0 left-0 right-0 h-1 bg-blue-500 origin-left z-50"
         style={{ scaleX: scrollYProgress }}
       />
-      
       <Navbar />
       <Banner />
       <About />
-      <Skills />
       <Timeline />
       <Projects />
+      <Skills />
       <Connect />
     </div>
   );
